f8e39f7f7b6fe245f3cd43b83bdd2fcf5425d7e5
Fazzoc <87571200+Fazzoc@users.noreply.github.com>
2024-08-23 17:13:21 +0100

    early logo thing

--- advanced_stuff.js
+++ advanced_stuff.js
@@ -1,8 +1,7 @@
- // Check if the hash is empty or just "#"
- window.addEventListener('load', function() {
-    if (window.location.hash === "") {
-        // If no hash, show default section
-        document.getElementById('default-section').style.visibility = "visible";
-        document.getElementById('default-section').style.opacity = 1;
-    }
+document.addEventListener("DOMContentLoaded", () => {
+    const hash = window.location.hash || "#start";
+    window.location.hash = hash;
+
+    const defaultSection = document.querySelector(hash);
+    defaultSection?.scrollIntoView();
 });
