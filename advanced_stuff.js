a0a503d0773d8be37b22d6d6d733e1b4dac5b6ff
Fazzoc <87571200+Fazzoc@users.noreply.github.com>
2024-08-27 13:00:36 +0100

    well, lucky there's a family guy

--- advanced_stuff.js
+++ advanced_stuff.js
@@ -5,3 +5,26 @@ document.addEventListener("DOMContentLoaded", () => {
     const defaultSection = document.querySelector(hash);
     defaultSection?.scrollIntoView();
 });
+
+function hideStartHash() {
+    if (window.location.hash === "#start") {
+      history.replaceState(null, null, window.location.href.split('#')[0]);
+    }
+  }
+  
+  window.addEventListener("load", hideStartHash);
+  
+  window.addEventListener("hashchange", hideStartHash);
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var logo = document.querySelector(".logo");
+    var audioPlayer = document.getElementById("audioPlayer");
+    var hasPlayed = false;
+
+    logo.addEventListener("click", function() {
+        if (!hasPlayed) {
+            audioPlayer.play();
+            hasPlayed = true;
+        }
+    });
+});
